5c5755e0b8573d539257c09033471a8c13385cfa
STASKE <STASKE4IT@gmail.com>
2024-05-02 13:40:09 +0500

    text

--- src/components/UI/Theme/ThemeContext.tsx
+++ src/components/UI/Theme/ThemeContext.tsx
@@ -1,23 +1,23 @@
-import { createContext, useContext, useState, useEffect, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
 
 export const ThemeContext = createContext<string>("light");
 
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<string>(localStorage.getItem("theme") || "light");
+  const [theme] = useState<string>(localStorage.getItem("theme") || "light");
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
   }, [theme]);
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
-
   return (
-    <ThemeContext.Provider value={theme}>
-      {children}
-    </ThemeContext.Provider>
+    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
   );
 };
--- src/components/UI/Theme/colors.ts
+++ src/components/UI/Theme/colors.ts
@@ -1,4 +1,3 @@
-import { string } from "yup";
 
 export const colors = {
     
